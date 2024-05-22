9cc828571fd4c7785a60717f677f89838de95c4c
Manoj-9755 <mdas925890@gmail.com>
2024-05-22 16:03:47 +0530

    seven

--- src/componets/bottom.jsx
+++ src/componets/bottom.jsx
@@ -9,11 +9,11 @@ export const Bottom = () => {
     <div>
       <div className=" w-full flex justify-between">
         <div className="w-[50%] p-5 ml-3.5">
-          <h1 className=" capitalize font-bold text-4xl">
+          <h1 className=" capitalize font-bold text-4xl my-[40px]">
             new style for latest collection
           </h1>
         </div>
-        <p className="w-[50%] font-semibold p-5 text-sm">
+        <p className="w-[50%] font-semibold p-5 text-sm my-[40px]">
           Discover our latest collection of clothing,shoes, and accessories that
           are perfect for any occasion .From casual wear to formal attire, we
           have everything you need to revamp you wardrobe and stay on trend
--- src/componets/nav.jsx
+++ src/componets/nav.jsx
@@ -1,7 +1,13 @@
 import { BsArchiveFill } from "react-icons/bs";
 import { VscAccount } from "react-icons/vsc";
 import { CiSearch } from "react-icons/ci";
+import { useState } from "react";
 export const Navbar = () => {
+     const [isTrue ,SetIstrue]= useState(true)
+    const handlecChange=()=>{
+      SetIstrue(false)
+    }
+
   return (
     <div>
       <div className="flex justify-between flex-nowrap w-full h-[70px]">
@@ -10,7 +16,7 @@ export const Navbar = () => {
             Joota <span className="capitalize text-red-500"> Shop</span>
           </h1>
         </div>
-          <div className=" w-[35%] flex justify-between   p-2 mr-7">
+          <div className=" w-[32%] flex justify-between   p-2 mr-7">
             <a href="/men" className="font-bold">Men</a>
             <a href="women" className="font-bold">Women</a>
             <a href="categories" className="font-bold">Categories</a>
@@ -20,12 +26,15 @@ export const Navbar = () => {
         
         <div className="  w-[30%] flex justify-end mr-7 mt-1  ">
             <div className="relative">
-              <input
-                className="  w-[135px] h-[33px] mt-1 px-7 rounded-3xl w-22 border-2 border-gray-300 placeholder:italic shadow-sm placeholder:text-sm"
+              <input onChange={handlecChange}
+                className="  w-[145px] h-[33px] mt-1 px-7 rounded-3xl w-22 border-2 border-gray-300 placeholder:italic shadow-sm placeholder:text-sm"
                 placeholder="Search..."
               />
               <div className="absolute top-3.5 left-[10px]  ">
-              <CiSearch  className=""/>
+                {
+              
+              isTrue&&<CiSearch  />
+                }
 
               </div>
             </div>
